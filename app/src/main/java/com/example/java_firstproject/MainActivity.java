cb95eb2908457db5cfe1465c737c3a1be00b2de1
prajkta <waingankar.prajkta27@gmail.com>
2025-05-25 01:27:16 +0530

    "initial commit"

---
+++ app/src/main/java/com/example/java_firstproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.java_firstproject;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+TextView txt1 , txt2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        txt1 = findViewById(R.id.txt1);
+        txt2 = findViewById(R.id.txt2);
+
+        txt2.setText("Get conflit here");
+
+    }
+}
